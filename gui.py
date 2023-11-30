5df7b2121add1a6f1f6e8eb4b7a2cea6a01e0572
sophiaBirch <birch.sophia87@gmail.com>
2023-11-29 19:42:34 -0600

    updated gui to use files to persist data, added name entry

--- gui.py
+++ gui.py
@@ -1,5 +1,5 @@
 from tkinter import *
-
+import csv
 ##a graphical user interface that sits in a window (base)
 class Gui():
 
@@ -13,18 +13,28 @@ class Gui():
 
         self.john_votes: int = 0
         self.jane_votes: int = 0
+        self.voted_users = set()
 
         self.vote_count_label = Label(base, text="John - 0 Jane - 0 Total - 0")
+        self.name_label = Label(base, text="Your Name:")
+        self.name_entry = Entry(base)
+        self.vote_label = Label(base, text="Num. votes:")
         self.vote_entry = Entry(base)
+
+        self.status_label = Label(base, text="")
         self.vote_john_button = Button(base, text="Add votes to John", command=self.vote_for_john)
         self.vote_jane_button = Button(base, text="Add votes to Jane", command=self.vote_for_jane)
         self.exit_button = Button(base, text="Exit", command=self.exit_app)
         self.incorrect_value_label = Label(base, text = "")
-
+        
         self.vote_count_label.pack()
+        self.name_label.pack()
+        self.name_entry.pack()
+        self.vote_label.pack()
         self.vote_entry.pack()
         self.vote_john_button.pack()
         self.vote_jane_button.pack()
+        self.status_label.pack()
         self.exit_button.pack()
         self.incorrect_value_label.pack()
 
@@ -32,17 +42,19 @@ class Gui():
         '''
             Adds votes to John and updates the vote count label
         '''
-        votes = self.get_number_of_votes()
-        self.add_votes_to_john(votes)
-        self.update_vote_count()
+        if self.check_and_save_vote("John"):
+            votes = self.get_number_of_votes()
+            self.add_votes_to_john(votes)
+            self.update_vote_count()
 
     def vote_for_jane(self) -> None:
         '''
             Adds votes to Jane and updates the vote count label
         '''
-        votes = self.get_number_of_votes()
-        self.add_votes_to_jane(votes)
-        self.update_vote_count()
+        if self.check_and_save_vote("Jane"):
+            votes = self.get_number_of_votes()
+            self.add_votes_to_jane(votes)
+            self.update_vote_count()
 
     def add_votes_to_john(self, votes) -> None:
         '''
@@ -55,6 +67,24 @@ class Gui():
             Increases amount of votes for jane
         '''
         self.jane_votes += votes
+
+    def check_and_save_vote(self, candidate) -> bool:
+        '''
+            Check if the user has already voted, save vote to CSV
+        '''
+        name = self.name_entry.get().strip()
+        if not name:
+            self.status_label.config(text="Please enter your name.")
+            return False
+
+        if name in self.voted_users:
+            self.status_label.config(text="You have already voted.")
+            return False
+
+        self.voted_users.add(name)
+        self.save_vote_to_csv(name, candidate)
+        self.status_label.config(text="Vote recorded. Thank you!")
+        return True
 
     def get_vote_count(self) -> int:
         '''
@@ -97,5 +127,13 @@ class Gui():
         except TypeError:
             self.incorrect_value_label.config(text=f"Please enter a positive number")
             return 0
+
+    def save_vote_to_csv(self, name, candidate) -> None:
+        '''
+            Save name and vote to csv file
+        '''
+        with open('votes.csv', 'a', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow([name, candidate, self.get_number_of_votes()])
 
 
--- test_gui.py
+++ test_gui.py
@@ -2,6 +2,11 @@ import unittest
 from tkinter import Tk
 from gui import Gui
 
+import unittest
+import os
+from tkinter import Tk
+from gui import Gui
+
 class TestGui(unittest.TestCase):
 
     def setUp(self) -> None:
@@ -10,12 +15,19 @@ class TestGui(unittest.TestCase):
         '''
         self.root = Tk()
         self.gui = Gui(self.root)
+        self.csv_file = 'votes.csv'
+
+        # Ensure a clean state for CSV file
+        if os.path.exists(self.csv_file):
+            os.remove(self.csv_file)
 
     def tearDown(self) -> None:
         '''
             Destroys the tester Gui class
         '''
         self.root.destroy()
+        if os.path.exists(self.csv_file):
+            os.remove(self.csv_file)
 
     def test_initial_values(self) -> None:
         '''
@@ -24,10 +36,44 @@ class TestGui(unittest.TestCase):
         self.assertEqual(self.gui.john_votes, 0)
         self.assertEqual(self.gui.jane_votes, 0)
 
+    def test_require_name_for_voting(self) -> None:
+        '''
+            Tests that a name is required before voting
+        '''
+        self.gui.vote_entry.insert(0, '3')
+        self.gui.vote_for_john()
+        self.assertNotEqual(self.gui.status_label['text'], "Vote recorded. Thank you!")
+
+    def test_prevent_duplicate_voting(self) -> None:
+        '''
+            Tests that a user cannot vote more than once
+        '''
+        self.gui.name_entry.insert(0, 'Alice')
+        self.gui.vote_entry.insert(0, '1')
+        self.gui.vote_for_john()
+        self.assertEqual(self.gui.status_label['text'], "Vote recorded. Thank you!")
+
+        self.gui.vote_for_jane()
+        self.assertEqual(self.gui.status_label['text'], "You have already voted.")
+
+    def test_csv_file_update(self) -> None:
+        '''
+            Test if the CSV file is updated with a new vote
+        '''
+        self.gui.name_entry.insert(0, 'Bob')
+        self.gui.vote_entry.insert(0, '1')
+        self.gui.vote_for_jane()
+
+        # Check if the CSV file contains the expected entry
+        with open(self.csv_file, 'r') as file:
+            last_line = file.readlines()[-1]
+            self.assertIn('Bob,Jane', last_line)
+
     def test_vote_for_john(self) -> None:
         '''
             Tests that the "vote john" functionality is working
         '''
+        self.gui.name_entry.insert(0, "Sophia")
         self.gui.vote_entry.insert(0, '3')  # Simulate entering 3 votes
         self.gui.vote_for_john()
         self.assertEqual(self.gui.john_votes, 3)
@@ -37,6 +83,7 @@ class TestGui(unittest.TestCase):
         '''
             Tests that the "vote jane" functionality is working
         '''
+        self.gui.name_entry.insert(0, "Sophia Birch")
         self.gui.vote_entry.insert(0, '3') # Simulate entering 3 votes
         self.gui.vote_for_jane()
         self.assertEqual(self.gui.jane_votes, 3)
@@ -46,6 +93,7 @@ class TestGui(unittest.TestCase):
         '''
             Simulate entering a non-integer
         '''
+        self.gui.name_entry.insert(0,"Sophia")
         self.gui.vote_entry.insert(0, 'abc') 
         self.gui.vote_for_john()
         self.assertEqual(self.gui.incorrect_value_label['text'], "Please enter a numerical value")
@@ -54,6 +102,7 @@ class TestGui(unittest.TestCase):
         '''
             Simulates entering a negative number
         '''
+        self.gui.name_entry.insert(0,"Sophia")
         self.gui.vote_entry.insert(0, -32)
         self.gui.vote_for_jane()
         self.assertEqual(self.gui.incorrect_value_label['text'], "Please enter a positive number")
